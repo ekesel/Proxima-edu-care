19c6da74fe841c07416a440d0d9fbda6d1a97b10
Ekaansh Sahni <ekaanshsahni@gmail.com>
2021-03-25 18:26:44 +0530

    Update settings.py

--- proximacare/settings.py
+++ proximacare/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c@*70=#&0o0ua1j-zfm+s@ij+&lyy*zh^#okcwi$^9n76#8#9u'
+SECRET_KEY = '***************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
